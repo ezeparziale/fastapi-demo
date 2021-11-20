afd9bee7a1d2e069a2cc326b4008621a923c27ea
Ezequiel Parziale <eparziale@gmail.com>
2021-11-20 16:37:16 -0300

    :construction: wip: new base model

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
@@ -12,6 +20,8 @@ def get_posts():
     return {"data": "This is your posts"}
 
 @app.post("/create_posts")
-def create_posts(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"data": "new post"}
+
