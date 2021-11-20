930516c77677e57f58d278153d53c7c1c9cbcb44
Ezequiel Parziale <eparziale@gmail.com>
2021-11-20 17:21:38 -0300

    :construction: wip: create post status code

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ async def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 1000000)
