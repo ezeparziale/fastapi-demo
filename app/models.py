f9cb753ecbe5d41c6151b302761cd6930a4fa371
Ezequiel Parziale <eparziale@gmail.com>
2021-11-21 19:49:19 -0300

    :construction: wip: model fix integer

--- app/models.py
+++ app/models.py
@@ -1,11 +1,10 @@
-from sqlalchemy import Column, Intenger, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean
 from .database import Base
-
 
 class Post(Base):
     __tablename__ = "posts"
 
-    id = Column(Intenger, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, default=True)
