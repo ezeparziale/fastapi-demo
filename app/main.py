9cb50f7190c51912033fcb0c8bb8952efb045d23
Ezequiel Parziale <eparziale@gmail.com>
2021-11-21 20:31:00 -0300

    :shirt: refactor: clean code

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
 from . import models
 from .database import engine, get_db
 from sqlalchemy.orm import Session
@@ -19,30 +15,9 @@ class Post(BaseModel):
     content: str
     published: bool = True
 
-while True:
-    try:
-        conn = psycopg2.connect(
-            host="localhost", 
-            database="fastapi", 
-            user="fastapi", 
-            password="fastapi", 
-            cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was succesfull!")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ", error)
-        time.sleep(2)
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {"data": posts}
 
 @app.get("/posts")
 def get_posts(db: Session = Depends(get_db)):
