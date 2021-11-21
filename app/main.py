83155101852126d11fee022fbf7a9786299a0b6f
Ezequiel Parziale <eparziale@gmail.com>
2021-11-21 19:44:06 -0300

    :shirt: refactor: clean code

--- app/main.py
+++ app/main.py
@@ -30,19 +30,6 @@ while True:
         print("Error: ", error)
         time.sleep(2)
 
-my_posts = [{"title": "AAA", "content": "aaa", "id": 1}, 
-            {"title": "BBB", "content": "bbb", "id": 2}]
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p["id"] == id:
-            return i
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -60,11 +47,6 @@ def create_posts(post: Post):
     new_post = cursor.fetchone()
     conn.commit()
     return {"data": new_post}
-
-@app.get("/posts/latest")
-def get_latest_post():
-    post = my_posts[len(my_posts)-1]
-    return post
 
 @app.get("/posts/{id}")
 def get_post(id: int):
