61aa488e6682bc6d9dcb71c9891c9429cbcb7169
Ezequiel Parziale <eparziale@gmail.com>
2021-11-21 20:29:36 -0300

    :construction: wip: update post

--- app/main.py
+++ app/main.py
@@ -78,12 +78,13 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, 
-                (post.title, post.content, post.published, str(id),))
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if updated_post == None:
+def update_post(id: int, post: Post, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post_to_update = post_query.first()
+    if post_to_update == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                         detail=f"Post with id {id} does not exist")
-    return {"data": updated_post}
+
+    post_query.update(post.dict(), synchronize_session=False)
+    db.commit()
+    return {"data": post_query.first()}
