26eee196ce64385188ae106cc7c56a0b4b160a52
Ezequiel Parziale <eparziale@gmail.com>
2021-11-21 19:54:43 -0300

    :construction: wip: test

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from typing import Optional
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from . import models
+from .database import engine, get_db
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -33,6 +38,10 @@ while True:
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    return {"status": "success"}
 
 @app.get("/posts")
 def get_posts():
