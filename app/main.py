84dc93eef461adb1137e6702ff7f9132cbea6238
Ezequiel Parziale <eparziale@gmail.com>
2021-11-21 19:08:36 -0300

    :construction: wip: db create posts

--- app/main.py
+++ app/main.py
@@ -55,10 +55,11 @@ def get_posts():
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, 
+        (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post}
 
 @app.get("/posts/latest")
 def get_latest_post():
