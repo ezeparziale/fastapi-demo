5af35eb8a98cfd52a17e7009c6aa78805b8b95ad
Ezequiel Parziale <eparziale@gmail.com>
2021-11-22 12:12:48 -0300

    :construction: wip: get user

--- app/main.py
+++ app/main.py
@@ -68,3 +68,12 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get("/users/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with: {id} does not exists")
+    
+    return user
