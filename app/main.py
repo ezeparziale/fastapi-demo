dd90f322e9050801bebd7ecec2aacb01e584cb73
Ezequiel Parziale <eparziale@gmail.com>
2021-11-22 12:04:49 -0300

    :zap: improvement: hash function

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
 from typing import Optional, List
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from random import randrange
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session
-from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -62,7 +60,7 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
-    hashed_password = pwd_context.hash(user.password)
+    hashed_password = utils.hash(user.password)
     user.password = hashed_password
 
     new_user = models.User(**user.dict())
