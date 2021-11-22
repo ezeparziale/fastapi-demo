d823fcaca86f4a2c389582863981e09ea78f90fa
Ezequiel Parziale <eparziale@gmail.com>
2021-11-22 13:25:18 -0300

    :construction: wip: get current user token

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,10 +1,11 @@
 from jose import JWSError, jwt
 from datetime import datetime, timedelta
-
-from jose.constants import Algorithms
 from jose.exceptions import JWTError
-
+from fastapi import status, HTTPException, Depends
 from . import schemas
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -29,3 +30,10 @@ def verify_access_token(token: str, credentials_exception):
     except JWTError:
         raise credentials_exception
 
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, 
+        detail=f"Could not validate credentials", 
+        headers={"WWW-Authenticate": "Bearer"})
+
+    return verify_access_token(token, credentials_exception)
