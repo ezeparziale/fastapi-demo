4ae71be991a1dca2b34e6cb257acbe5c32a1ee3a
Ezequiel Parziale <eparziale@gmail.com>
2021-11-22 14:13:41 -0300

    :construction: wip: login with schema

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -9,7 +9,7 @@ router = APIRouter(
     tags=["Authentication"]
 )
 
-@router.post("/")
+@router.post("/", response_model=schemas.Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
