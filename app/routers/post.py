24fc56febeb79aae92fe6be3c3118554fc281979
Ezequiel Parziale <eparziale@gmail.com>
2021-12-12 14:35:44 -0300

    :construction: wip: get posts with owner id

--- app/routers/post.py
+++ app/routers/post.py
@@ -14,7 +14,7 @@ router = APIRouter(
 
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    posts = db.query(models.Post).all()
+    posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
