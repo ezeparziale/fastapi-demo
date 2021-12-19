f7e7dd5c2b738a53854905297ebbe001fea4150d
Ezequiel Parziale <eparziale@gmail.com>
2021-12-19 20:28:37 -0300

    :construction: wip: add cors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
 
 # models.Base.metadata.create_all(bind=engine)
 
+origins = ["*"]  # "*" para DEV, completar con URL productiva para PROD
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[origins],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
