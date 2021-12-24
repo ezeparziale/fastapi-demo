0b734ecfe98fafbc09e76e7c19595a48b65160de
Ezequiel Parziale <eparziale@gmail.com>
2021-12-23 23:54:49 -0300

    :construction: wip: add posts test

--- test/conftest.py
+++ test/conftest.py
@@ -8,6 +8,8 @@ from app.config import settings
 from app.database import get_db
 from app.database import Base
 import pytest
+from app.oauth2 import create_access_token
+from app import models
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
 
@@ -50,3 +52,55 @@ def test_user(client):
     new_user = res.json()
     new_user['password'] = user_data['password']
     return new_user
+
+@pytest.fixture
+def test_user2(client):
+    user_data = {"email": "abc2@test.com",
+                 "password": "abc123"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    print(res.json())
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id": test_user['id']})
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+    return client
+
+
+@pytest.fixture
+def test_posts(test_user, test_user2, session):
+    posts_data = [{
+        "title":"Title_1",
+        "content":"Content_1",
+        "owner_id": test_user['id']
+    },
+    {
+        "title":"Title_2",
+        "content":"Content_2",
+        "owner_id": test_user['id']
+    },
+    {
+        "title":"Title_2",
+        "content":"Content_2",
+        "owner_id": test_user2['id']
+    }]
+
+    def create_user_model(post):
+        return models.Post(**post)
+
+    post_map = map(create_user_model, posts_data)
+    posts = list(post_map)
+    session.add_all(posts)
+    session.commit()
+    post = session.query(models.Post).all()
+    return post
