526ff95cdef33ff02e5ff42c384ab82b837eca2d
Ezequiel Parziale <eparziale@gmail.com>
2021-12-25 19:21:33 -0300

    :zap: improvement: root endpoint welcome

--- app/main.py
+++ app/main.py
@@ -22,3 +22,7 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+@app.get("/")
+def root():
+    return {"message": "Bienvenido a la demo de fastapi!!!!"}
