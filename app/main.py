1d1d9ce5e8ba30d87e41c088a6acab31fa8afbc2
Ezequiel Parziale <eparziale@gmail.com>
2022-12-30 18:22:25 -0300

    :rocket: update: routers order

--- app/main.py
+++ app/main.py
@@ -23,9 +23,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(auth.router)
 app.include_router(post.router)
 app.include_router(user.router)
-app.include_router(auth.router)
 app.include_router(vote.router)
 
 
