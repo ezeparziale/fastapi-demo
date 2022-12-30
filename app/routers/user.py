50b3eecd4ebe599dd793db1596fa9d4756dbbd8e
Ezequiel Parziale <eparziale@gmail.com>
2022-12-30 18:00:20 -0300

    :rocket: update: add check user exists

--- app/routers/user.py
+++ app/routers/user.py
@@ -10,6 +10,12 @@ router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    user_exist = db.query(models.User).filter(models.User.email == user.email).first()
+    if user_exist:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"User exists",
+        )
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
     new_user = models.User(**user.dict())
