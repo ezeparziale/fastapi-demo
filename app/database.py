0ea846a0cef5964cd0a372ca33f168c588273a51
Ezequiel Parziale <eparziale@gmail.com>
2023-04-18 21:57:23 -0300

    :card_file_box: update: change create engine env var

--- app/database.py
+++ app/database.py
@@ -1,12 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-from .config import settings
+from app.config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI)  # type: ignore[arg-type]
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
